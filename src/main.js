6f475cff7c63fab22ce3ef0da73489750ebb7f6c
inthere <inthere@qq.com>
2024-03-11 02:35:01 +0800

    feat: add lazy instruction

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './routes'
+import lazy from './hooks/lazy'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(lazy)
+.use(router)
+.mount('#app')
