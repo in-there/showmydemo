7fe77f53b23bd2401eb8eb6c2549578332c12f80
inthere <inthere@qq.com>
2024-03-11 02:30:54 +0800

    feat: add img component

---
+++ src/components/Img.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="img">
+        <div class="img-loading">
+            <Loading></Loading>
+        </div>
+        <img class="fade" :src="img">
+    </div>
+</template>
+<script setup >
+import Loading from './Loading.vue';
+const props = defineProps(['img'])
+</script>
+<style lang="scss" scoped >
+.img{
+    position: relative;
+    width: 100%;
+    &-loading{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+    .fade{
+        visibility: hidden;
+    }
+    .hide{
+        display: none;
+    }
+}
+</style>
